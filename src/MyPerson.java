2692ddda394591ce54c70578110e53637cf91340
Mahadevan <mahadevanpillai651@gmail.com>
2025-08-24 15:23:16 +0530

    Add Java code

--- src/MyPerson.java
+++ src/MyPerson.java
@@ -2,6 +2,8 @@ public class MyPerson {
     public static void main(String[] args) {
         Person p=new Person("Ghaziabad");
         p.getAddress();
-        SchoolStudent st =new SchoolStudent("Ghaziabad", )
+        SchoolStudent st=new SchoolStudent("Ghaziabad","B.tech");
+        st.getAddress();
+        st.getCourse();
     }
 }
--- src/Person.java
+++ src/Person.java
@@ -1,9 +1,11 @@
 public class Person {
-    public String address;
+    private String address;
+
     public Person(String address) {
         this.address = address;
     }
-    public void getAddress() {
+    public void getAddress()
+    {
         System.out.println("Address is "+address);
     }
 }
