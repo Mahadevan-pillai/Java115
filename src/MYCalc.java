8f21ecc9cf1cd0631b0ddaf47799936a01ce75bf
Mahadevan <mahadevanpillai651@gmail.com>
2025-08-26 11:13:18 +0530

    Add Java code

---
+++ src/MYCalc.java
@@ -0,0 +1,8 @@
+public class MYCalc {
+    public static void main(String[] args) {
+        Calculator cl = new Calculator();
+        cl.Area(12.3,23.4);
+        cl.Area(12.25);
+        cl.Area(12,34,45);
+    }
+}
